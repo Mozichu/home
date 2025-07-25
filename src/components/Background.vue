c7555f3e3c50a6fc8f8efb9f383f7e9a0b723684
与祁 <103705462+Mozichu@users.noreply.github.com>
2025-07-25 08:34:36 +0800

    Update Background.vue

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -40,7 +40,8 @@
 import { mainStore } from "@/store";
 import { Error } from "@icon-park/vue-next";
 import { ref, computed, defineEmits, watch, onMounted, onBeforeUnmount } from 'vue';
-import { ElMessage, h } from 'element-plus';
+import { ElMessage } from 'element - plus';
+import { h } from 'vue'; 
 
 const store = mainStore();
 const bgUrl = ref(null);
